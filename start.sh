8fab45789d3a950476c6d8d34303da2047412d4f
Amarkh1 <amarkhalid530@gmail.com>
2025-06-09 10:34:30 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Install system dependencies if needed
+if [ ! -f /usr/bin/espeak ]; then
+    apt-get update && apt-get install -y espeak espeak-data libespeak1 libespeak-dev ffmpeg
+fi
+
+# Start the bot
+python ttp7.py
